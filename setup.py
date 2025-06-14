04fb46c52026276df7ef9fcaf0276f804ff125b2
Keshava Munegowda <keshava.gowda@gmail.com>
2025-06-14 16:13:44 +0530

    Add setup.py
    Signed-off-by: Keshava Munegowda <keshava.gowda@gmail.com>

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='sbk-charts',
+    version='1.25.6',
+    packages=['charts'],
+    url='https://github.com/kmgowda/sbk-charts',
+    license='Apache License Version 2.0',
+    author='KMG',
+    author_email='keshava.gowda@gmail.com',
+    description='SBK Charts'
+)
