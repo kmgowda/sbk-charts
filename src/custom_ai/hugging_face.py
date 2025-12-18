402746f3979c66126414e79b69b7ba9346f26e1f
Keshava Munegowda <keshava.gowda@gmail.com>
2025-12-18 21:58:00 +0530

    Add base methods for generative AI
    Signed-off-by: Keshava Munegowda <keshava.gowda@gmail.com>

---
+++ src/custom_ai/hugging_face.py
@@ -0,0 +1,19 @@
+#!/usr/local/bin/python3
+# Copyright (c) KMG. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+##
+
+from src.genai.genai import SbkGenAI
+
+class HuggingFace(SbkGenAI):
+    def __init__(self):
+        super().__init__()
+        pass
+
+    def get_throughput_mb_analysis(self, throughput, prompt_text):
+        pass
---
+++ src/genai/genai.py
@@ -0,0 +1,16 @@
+#!/usr/local/bin/python3
+# Copyright (c) KMG. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+##
+
+class SbkGenAI:
+    def __init__(self):
+        pass
+
+    def get_throughput_mb_analysis(self, throughput, prompt_text):
+        pass
