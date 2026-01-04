298e02326e84097c3240780e656a233fa17198b5
Keshava Munegowda <keshava.gowda@gmail.com>
2026-01-04 21:18:35 +0530

    Set the next release version
    Signed-off-by: Keshava Munegowda <keshava.gowda@gmail.com>

--- src/main/sbk_version.py
+++ src/main/sbk_version.py
@@ -1,1 +1,11 @@
-__sbk_version__="2.25.12.0"
+#!/usr/local/bin/python3
+# Copyright (c) KMG. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+##
+
+__sbk_version__="3.26.X.0"
